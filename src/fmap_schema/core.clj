17a2b99324533dd26b25731ec48c9db209439585
Jessica Kerr <jessitron@outpace.com>
2015-02-23 14:50:57 +0000

    Fixed test

--- src/fmap_schema/core.clj
+++ src/fmap_schema/core.clj
@@ -1,6 +1,7 @@
 (ns fmap-schema.core
   (require [schema.core :refer [Schema subschema-walker explain]]
-           [schema.macros :as macros]))
+           [schema.macros :as macros]
+           [schema.utils :as utils]))
 
 (clojure.core/defrecord FMap [f schema]
   Schema
@@ -8,7 +9,11 @@
           (let [sub-walker (subschema-walker schema)]
             (clojure.core/fn [x]
               (try
-                (sub-walker (f x))
+                (let [result (sub-walker (f x))]
+                  ( if (utils/error? result)
+                    result ;;todo: add to error
+                    x)
+                  )
                 (catch Exception e
                    (macros/validation-error this x (list 'fmap f (explain schema)) (str "Exception thrown during function evaluation: " (.getMessage e))))))))
   (explain [this] (list 'fmap f (explain schema))))
